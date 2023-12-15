d5793d7677abdbc02a1536217d27c903acb0872a
Theo2801 <theologos.bourloglou@gmail.com>
2023-12-15 23:20:43 +0200

    [feat] Add encryption to passwords

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -39,7 +39,7 @@ if ($user) {
     // If not, save one to the database, and then log the user in, and redirect.
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
 
     // mark that the user has logged in
